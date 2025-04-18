947ce9ac1f11358b1e6d9fb420a4b9b7a20bc89c
Elliot <ayg8726@outlook.com>
2025-04-18 20:43:53 +0530

    feat: initial backend setup

---
+++ backend-ts/src/models/Contact.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  subject: String,
+  message: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default Contact;
+
