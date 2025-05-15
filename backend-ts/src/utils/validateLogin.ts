3718f9eea26b7cf5de320179d82b9dd817e854b8
Elliot <ayg8726@outlook.com>
2025-05-15 23:58:57 +0530

    helper function for checking logging credentials

---
+++ backend-ts/src/utils/validateLogin.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import User from '../models/Register';
+
+export async function checkCredential(email: string, password: string) {
+    const user = await User.findOne({ email, password });
+    if(!user) {
+        return "Invalid email or password";
+    }
+    return null;
+}
