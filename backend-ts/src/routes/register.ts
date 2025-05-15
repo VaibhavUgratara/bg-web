f999d2dee40ee410579403c2072a8886e9eaf803
Elliot <ayg8726@outlook.com>
2025-05-15 21:40:30 +0530

    Input validation done

--- backend-ts/src/routes/register.ts
+++ backend-ts/src/routes/register.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/Register';
+import { validateRegisterInput } from '../utils/validateRegister';
 
 const registerRoute = express.Router();
 
@@ -7,14 +8,29 @@ registerRoute.post('/', async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
+
+    const error = validateRegisterInput( name, email, password);
+    if(error) {
+       res.status(400).json( {error} );
+       return;
+    }
+
+    const existingUser = await User.findOne({ email });
+    if(existingUser) {
+        res.status(400).json({error: "Email already registered"});
+        return;
+    }
+
     const newUser = new User({ name, email, password });
     await newUser.save();
 
     res.status(201).json({ message: 'User Registered Successfully' });
   } catch (err) {
-    console.error('Error! Registering', err);
-    res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error! Registering', err);
+      res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+
 
 export default registerRoute;
